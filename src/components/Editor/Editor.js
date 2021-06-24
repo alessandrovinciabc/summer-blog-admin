27f6d6b2f8d1964b53fc0a64b242ef18ea9ca76e
Alessandro Vinci <alessandrovinciabc@gmail.com>
2021-06-24 21:48:43 +0200

    Create view for making posts, editor component

---
+++ src/components/Editor/Editor.js
@@ -0,0 +1,51 @@
+import EditorJS from '@editorjs/editorjs';
+
+/* Tools */
+import Header from '@editorjs/header';
+import SimpleImage from '@editorjs/simple-image';
+import List from '@editorjs/list';
+import Embed from '@editorjs/embed';
+
+import React, { useEffect, useRef } from 'react';
+
+const Editor = React.forwardRef((props, ref) => {
+  let editor = useRef();
+  let editorContainer = useRef();
+
+  useEffect(() => {
+    const editorjs = new EditorJS({
+      placeholder: 'Type something...',
+      data: props.data ?? {},
+      holder: editorContainer.current,
+      tools: {
+        header: {
+          class: Header,
+          config: {
+            placeholder: 'Enter a header',
+            levels: [1, 2, 3, 4, 5, 6],
+            defaultLevel: 2,
+          },
+        },
+        image: SimpleImage,
+        list: {
+          class: List,
+          inlineToolbar: true,
+        },
+        embed: {
+          class: Embed,
+          inlineToolbar: true,
+        },
+      },
+    });
+
+    editor.current = editorjs;
+  }, []);
+
+  useEffect(() => {
+    ref.current = editor.current;
+  }, [ref]);
+
+  return <div ref={editorContainer}></div>;
+});
+
+export default Editor;
--- src/pages/CreateView.js
+++ src/pages/CreateView.js
@@ -1,7 +1,82 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+
+import Loader from '../components/Loader';
+import Editor from '../components/Editor/Editor';
 
 function CreateView(props) {
-  return <h1>Create</h1>;
+  let [titleInput, setTitleInput] = useState('');
+
+  let [isSaving, setIsSaving] = useState(false);
+  let editorRef = useRef();
+
+  async function handleSave(e) {
+    e.preventDefault();
+    setIsSaving(true);
+
+    let data;
+    try {
+      data = await editorRef.current.save();
+    } catch (err) {
+      alert('An error occurred');
+    }
+
+    /* TODO: save data to DB */
+    console.log(data);
+
+    setIsSaving(false);
+  }
+
+  function resetEditor() {
+    setTitleInput('');
+    editorRef.current.blocks.clear();
+  }
+
+  return (
+    <Container fluid>
+      <h1 className="text-center mt-3">Create</h1>
+      <div className="d-flex flex-column align-items-center">
+        {isSaving && (
+          <div className="my-4">
+            <Loader />
+          </div>
+        )}
+        <br />
+        <Form
+          onSubmit={handleSave}
+          className="w-100 d-flex flex-column align-items-center"
+        >
+          <Form.Control
+            name="title"
+            type="text"
+            required
+            maxLength="60"
+            autoComplete="off"
+            className="title-input"
+            value={titleInput}
+            onChange={(e) => {
+              setTitleInput(e.target.value);
+            }}
+          />
+
+          <div className="mt-4">
+            <Button type="submit" className="mr-3" variant="warning">
+              Add Post to Blog
+            </Button>
+            <Button onClick={resetEditor} variant="danger">
+              Reset
+            </Button>
+          </div>
+        </Form>
+      </div>
+      <Container fluid className="mt-4">
+        <Editor ref={editorRef} />
+      </Container>
+    </Container>
+  );
 }
 
 export default CreateView;
